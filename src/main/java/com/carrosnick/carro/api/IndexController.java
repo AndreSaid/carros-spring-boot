c00f3131f21385aa0840ce88c4976271c8555a27
Andre Said <andresaid123@hotmail.com>
2020-09-19 10:40:39 -0300

    Testing GET,POST,PUT,DELETE requests

--- src/main/java/com/carrosnick/carro/api/IndexController.java
+++ src/main/java/com/carrosnick/carro/api/IndexController.java
@@ -1,6 +1,9 @@
 package com.carrosnick.carro.api;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
 	@GetMapping
-	public String hello() {
-		return "Hello Spring Boot";
+	public String get() {
+		return "Get Spring Boot";
 	}
 
-
-	@GetMapping("/test")
-	public String test() {
-		return "Hello Spring Test";
+	@PostMapping
+	public String post() {
+		return "Post Spring Boot";
+	}
+	
+	@PutMapping
+	public String put() {
+		return "Put Spring Boot";
+	}
+	
+	@DeleteMapping
+	public String delete() {
+		return "Delete Spring Boot";
 	}
 
 }
