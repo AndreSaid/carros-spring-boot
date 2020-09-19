43a75b68f5cb8ec0ecfe1dd561b601833f64a907
Andre Said <andresaid123@hotmail.com>
2020-09-19 10:31:25 -0300

    Creating first GET

---
+++ src/main/java/com/carrosnick/carro/api/IndexController.java
@@ -0,0 +1,22 @@
+package com.carrosnick.carro.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+	@GetMapping
+	public String hello() {
+		return "Hello Spring Boot";
+	}
+
+
+	@GetMapping("/test")
+	public String test() {
+		return "Hello Spring Test";
+	}
+
+}
